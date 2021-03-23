35bcedb8fbe93fd69bda5c3c34bcaf92f5ac3dc9
Zera57 <svetik.avtonomov@gmail.com>
2021-03-23 14:24:07 +0300

    Upgraded scripts

--- setup.sh
+++ setup.sh
@@ -5,7 +5,4 @@ minikube addons enable metallb
 eval $(minikube docker-env)
 kubectl apply -f metallb.yaml
 
-#NGINX
-bash nginx.sh
-bash wordpress.sh
-
+bash start.sh
---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+bash nginx.sh
+bash wordpress.sh
+
+minikube dashboard
