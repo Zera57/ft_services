4def9850f49da9d44cc2b1cc7a24a7f71547b4f2
Zera57 <svetik.avtonomov@gmail.com>
2021-03-31 18:09:57 +0300

    fixed port for influxdb

--- srcs/telegraf/conf/start.sh
+++ srcs/telegraf/conf/start.sh
@@ -1,6 +1,7 @@
 #!bin/sh
 
-/telegraf-1.18.0/usr/bin/telegraf -config /telegraf-1.18.0/etc/telegraf/telegraf.conf
+#/telegraf-1.18.0/usr/bin/telegraf -sample-config --input-filter docker --output-filter influxdb > /telegraf-1.18.0/etc/telegraf/telegrafnew.conf
+#/telegraf-1.18.0/usr/bin/telegraf -sample-config /telegraf-1.18.0/etc/telegraf/telegrafnew.conf
 
-# nginx -g 'daemon off;'
+nginx -g 'daemon off;'
 sh
